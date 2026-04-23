31f1ead94071810c456d5853cc66858606fd0ab0
Lilian <lilian@cher-ami.tv>
2026-04-23 09:47:04 +0200

    activate plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,22 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
-export default config;
+export default () => ({
+  navigation: {
+    enabled: true,
+    config: {
+      contentTypes: [
+        "api::page.page"
+      ],
+    },
+  },
+  graphql: { enabled: true },
+  upload: {
+    enabled: true,
+    config: {
+      breakpoints: {
+        xlarge: { breakpoint: 1920, formats: ["webp", "avif"] },
+        large: { breakpoint: 1680, formats: ["webp", "avif"] },
+        medium: { breakpoint: 1280, formats: ["webp", "avif"] },
+        small: { breakpoint: 640, formats: ["webp", "avif"] },
+      },
+    },
+  },
+});
